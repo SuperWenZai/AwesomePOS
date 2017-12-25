c0db7fe4a5546fad66106ca1aa287d85bfcafd5b
SupermanWenZai <34673339+SupermanWenZai@users.noreply.github.com>
2017-12-25 11:50:05 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <left-nav></left-nav>
+    <div class="main">
+      <keep-alive>
+         <router-view></router-view>
+      </keep-alive>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import leftNav from '@/components/common/leftNav'
+export default {
+  name: 'app',
+  data(){
+    return{}
+  },
+  components : {
+    leftNav
+  }
+}
+</script>
+
+<style lang="less" scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  .main {
+    height: 100%;
+    width: 95%;
+    float: right;
+    background-color: #EFF2F7;
+    overflow: hidden;
+  }
+}
+</style>
+
